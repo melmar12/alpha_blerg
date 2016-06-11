bd211b2da24bb3bc12add24e9af44e0fe8fd773f
Melissa Martinez <melmar12@live.com>
2016-06-10 23:49:17 -0500

    built authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   # post 'users', to: 'users#create'
   resources :users, :except => [:new]
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
